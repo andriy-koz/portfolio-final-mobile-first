5941f393dd7ef4ce15905d3e229027e2486ba5d6
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-01-12 02:43:18 -0300

    init: nextjs typescript tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Koz Dev - Desarrollo de software</title>
+        <meta
+          name='description'
+          content='Diseño y desarrollo de aplicaciones, sitios web, CRM y software en general'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main></main>
+    </>
+  );
+}
