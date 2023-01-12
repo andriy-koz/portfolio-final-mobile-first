72f1801b328628fa28ab5dc660ec053a58e67cf8
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-01-12 16:53:31 -0300

    style: hero animations

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -9,16 +9,18 @@ const Hero = () => {
       </div>
       <div className='absolute top-[35%] w-full text-center'>
         <h1 className='mb-12 text-white'>
-          <span className='block text-5xl bg-green-400/75 w-fit mx-auto px-1 -skew-x-12'>
+          <span className='block text-5xl bg-green-400/75 w-fit mx-auto px-1 -skew-x-12 animate-from-left'>
             <div className='skew-x-12'>
               <span className='tracking-tighter text-6xl'>KOZ</span> DEV
             </div>
           </span>
-          <span className='block text-xl font-bold uppercase w-fit mx-auto px-1 bg-green-400/75 -skew-x-12'>
+          <span className='block text-xl font-bold uppercase w-fit mx-auto px-1 bg-green-400/75 -skew-x-12 animate-from-right'>
             Diseño y desarrollo de software
           </span>
         </h1>
-        <ButtonCta link='#' text='explora nuestos proyectos' />
+        <div className='animate-from-bottom'>
+          <ButtonCta link='#' text='explora nuestos proyectos' />
+        </div>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'from-left': {
+          '0%': { transform: 'translateX(-100%); opacity:0' },
+          '75%': { transform: 'translateX(10%)' },
+          '100%': { transform: 'translateX(0); opacity:1' },
+        },
+        'from-right': {
+          '0%': { transform: 'translateX(100%); opacity:0' },
+          '75%': { transform: 'translateX(-10%)' },
+          '100%': { transform: 'translateX(0); opacity:1' },
+        },
+        'from-bottom': {
+          '0%': { transform: 'translateY(30%); opacity:0' },
+          '75%': { transform: 'translateY(-10%)' },
+          '100%': { transform: 'translateY(0); opacity:1' },
+        },
+      },
+      animation: {
+        'from-left': 'from-left .8s ease-out',
+        'from-right': 'from-right .8s ease-out',
+        'from-bottom': 'from-bottom .8s ease-out',
+      },
+    },
   },
   plugins: [],
 };
